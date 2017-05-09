f93639504f21ac9127e7dfcea01b84df4edc9717
xueli81993@gmail.com <xueli81993@gmail.com>
2017-05-09 02:06:28 -0400

    d

--- traditional method.py
+++ traditional method.py
@@ -9,6 +9,7 @@ D2 = preprocess.get_data(data)
 X = preprocess.get_X(D2)
 y = pd.DataFrame(preprocess.get_target(D2))
 
+
 X_train, X_test = X[:int((0.8*len(X)))], X[int((0.8*len(X))):]
 y_train, y_test = y[:int((0.8*len(X)))], y[int((0.8*len(X))):]
 
@@ -21,13 +22,15 @@ def svm():
     print 'SVM accuracy :' , accuracy
     print 'SVM trainset accuracy:', f1_score(y_train, clf.predict(X_train), average='macro')
 
-def randomforest():
+
+def randomforest(n, depth):
     from sklearn.ensemble import RandomForestClassifier
     from sklearn.metrics import f1_score
-    clf = RandomForestClassifier(n_estimators=100, max_depth= 100).fit(X_train, y_train)
+    clf = RandomForestClassifier(n_estimators=n, max_depth= depth).fit(X_train, y_train)
     accuracy = f1_score(y_test, clf.predict(X_test), average='macro')
     print 'RF accuracy :' , accuracy
     print 'RF train accuracy:' ,f1_score(y_train, clf.predict(X_train), average='macro')
+
 
 def NB():
     from sklearn.naive_bayes import GaussianNB
@@ -38,9 +41,13 @@ def NB():
     print 'NB train accuracy:', f1_score(y_train, gnb.predict(X_train), average='macro')
 
 
+
 if __name__ == "__main__":
     svm()
-    randomforest()
+    randomforest(100, 100)
+    randomforest(100, 50)
     NB()
 
-
+    X_less = pd.merge(X.iloc[:, 0:25], X.iloc[:, 1024:1049],how='outer', left_index=True, right_index=True)
+    X_train, X_test = X_less[:int((0.8 * len(X)))], X_less[int((0.8 * len(X))):]
+    randomforest(100,50)
